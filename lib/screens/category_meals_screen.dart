5de72899e00547b9c48dbfee60127527f1b198e5
Thomas <lefoux82@gmail.com>
2021-03-17 18:18:08 +0100

    Replace in new folder

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../models/meal.dart';
+import '../widgets/meal_item.dart';
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle, );
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    //retrouver la recette par rapport à sa categorie
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      // Creation de la liste des recettes
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            imageUrl:  categoryMeals[index].imageUrl,
+            title: categoryMeals[index].title,
+            duration: categoryMeals[index].duration,
+            affordability: categoryMeals[index].affordability,
+            complexity: categoryMeals[index].complexity,
+          );
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
