ed1d31569815710d517fd95ec6317eace0cbd1d9
Thomas <lefoux82@gmail.com>
2021-03-17 13:55:55 +0100

    create view for page 'recipes'

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('les recettes'),
+      ),
+      body: Center(
+        child: Text('ecrans recettes de la categorie selectionner'),
+      ),
+    );
+  }
+}
