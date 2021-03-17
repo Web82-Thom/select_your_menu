0cd4a102e924229f30400c3e00420eae21246ead
Thomas <lefoux82@gmail.com>
2021-03-17 14:19:01 +0100

    import, put data final and construteur, put title appBar

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:select_your_menu/category_item.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealsScreen(this.categoryId, this.categoryTitle, );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('les recettes'),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text('ecrans recettes de la categorie selectionner'),
