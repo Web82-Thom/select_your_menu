8f461fddda814885746a2acb0b6e45d2e2196602
Thomas <lefoux82@gmail.com>
2021-03-17 10:31:20 +0100

    add padding indentationy

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -12,13 +12,14 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('DeliMeal'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         //jappel dans mon children ma liste
         children: DUMMY_CATEGORIES
-            .map((catData) => CategoryItem(
-                  catData.title,
-                  catData.color,
-                ))
-            .toList(),
+          .map((catData) => CategoryItem(
+                catData.title,
+                catData.color,
+              ))
+          .toList(),
         //argument obligatoire gridDelegate donne du style au gridView
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
