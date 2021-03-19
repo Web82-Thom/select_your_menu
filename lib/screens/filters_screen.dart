47089d9ad8630493d89e0e7314812b9bfd6b3034
Thomas <lefoux82@gmail.com>
2021-03-19 10:33:26 +0100

    create a list of swicth with consctrutor

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -2,9 +2,35 @@ import 'package:flutter/material.dart';
 
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   //construction de ma route
   static const routeName = '/filtres';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  //stocke les elements
+  bool _glutenFree = false;
+  bool _vegetarian = false;
+  bool _vegan = false;
+  bool _lactoseFree = false;
+
+  //constructeur des listes de switch
+  Widget _buildSwitchListTile(
+    String title,
+    String description,
+    bool currentValue,
+    Function updateValue,
+  ) {
+    return SwitchListTile(
+      title: Text(title),
+      value: currentValue,
+      onChanged: updateValue,
+      subtitle: Text(description),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +39,60 @@ class FiltersScreen extends StatelessWidget {
         title: Text('Mes filtres'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Hello',),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Ajuster vos préférences',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Expanded(
+            child: ListView(children: <Widget>[
+              _buildSwitchListTile(
+                'Sans Glutten', 
+                'Uniquement les recettes sans glutten',
+                _glutenFree,
+                (newValue) {
+                  setState(() {
+                    _glutenFree = newValue;
+                  });
+                },
+              ),
+              _buildSwitchListTile(
+                'Végétarien', 
+                'Uniquement les recettes végétariennes',
+                _vegetarian,
+                (newValue) {
+                  setState(() {
+                    _vegetarian = newValue;
+                  });
+                },
+              ),
+              _buildSwitchListTile(
+                'Végan', 
+                'Uniquement les recettes végannes',
+                _vegan,
+                (newValue) {
+                  setState(() {
+                    _vegan = newValue;
+                  });
+                },
+              ),
+              _buildSwitchListTile(
+                'Sans lactose', 
+                'Uniquement les recettes sans lactose',
+                _lactoseFree,
+                (newValue) {
+                  setState(() {
+                    _lactoseFree = newValue;
+                  });
+                },
+              ),
+            ]),
+          ),
+        ],
       ),
     );
   }
